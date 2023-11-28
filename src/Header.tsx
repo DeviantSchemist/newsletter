23aa141075ce3ca0ace0b38ccafb5333efab6f3f
Michael Ngo <michaelngo1337@yahoo.com>
2023-11-28 15:48:08 -0800

    render desktop image

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,17 @@
 import MobileLogo from "./assets/images/illustration-sign-up-mobile.svg";
+import DesktopLogo from "./assets/images/illustration-sign-up-desktop.svg";
+import MediaQuery from "react-responsive";
 
-const Header = () => <img src={MobileLogo} alt="Mobile Banner" className="block" />
+const Header = () => {
+
+  return (
+    <>
+      <img src={MobileLogo} alt="Mobile Banner" className="block lg:hidden" />
+      <MediaQuery minWidth={1024}>
+        <img src={DesktopLogo} alt="Desktop Banner" className="block" />
+      </MediaQuery>
+    </>
+  )
+}
 
 export default Header
