30c144a8cc75f2be548adcb628cc8bc95ea791d9
Michael Ngo <michaelngo1337@yahoo.com>
2023-11-29 18:14:08 -0800

    fix height of Thanks component for desktop view

--- src/Thanks.tsx
+++ src/Thanks.tsx
@@ -5,7 +5,7 @@ const Thanks = (props) => {
 
   return (
     <div className="lg:pt-[280px] lg:pb-[280px] lg:pl-[468px] lg:pr-[468px] lg:bg-[#36384D] lg:h-screen lg:flex lg:justify-center">
-      <div className="lg:bg-white lg:rounded-[36px] pl-[24px] lg:pl-[64px] pr-[24px] lg:pr-[64px] pt-[149px] lg:pt-[48px] pb-[40px] lg:pb-[64px] flex flex-col font-Roberto items-start h-screen justify-between lg:h-auto lg:w-[504px] lg:h-[520px] lg:justify-normal">
+      <div className="lg:bg-white lg:rounded-[36px] pl-[24px] lg:pl-[64px] pr-[24px] lg:pr-[64px] pt-[149px] lg:pt-[48px] pb-[40px] lg:pb-[64px] flex flex-col font-Roberto items-start h-screen justify-between lg:w-[504px] lg:h-[520px] lg:justify-normal">
         <div className="">
           <img src={SuccessLogo} alt="Successful submission" className="mb-[40px]" />
           <p className="font-RobertoBold text-[40px] font-bold text-left text-[#242742] mb-[24px] leading-10">Thanks for subscribing!</p>
