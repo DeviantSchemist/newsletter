0b2ac88d529caa90c5d2bec053a8321a2c633ca9
Michael Ngo <michaelngo1337@yahoo.com>
2023-11-29 16:07:18 -0800

    make button change color on hover for Email component

--- src/Email.tsx
+++ src/Email.tsx
@@ -1,4 +1,4 @@
-
+import MediaQuery from "react-responsive"
 
 const Email =  (props) => {
 
@@ -9,9 +9,15 @@ const Email =  (props) => {
       <div className="rounded-lg border border-[#19182b]/25 mb-[24px]">
         <p className="opacity-50 text-base text-left text-[#242742] pt-[16px] pb-[16px] pl-[24px]">email@company.com</p>
       </div>
-      <button onClick={props.handleClick} className="rounded-lg bg-[#242742] pt-[18px] pb-[14px]">
+      <button onClick={props.handleClick} className="rounded-lg bg-[#242742] pt-[18px] pb-[14px] lg:hidden">
         <span className="text-base font-bold text-center text-white font-RobertoBold">Subscribe to monthly newsletter</span>
       </button>
+      
+      <MediaQuery minWidth={1024}>
+        <button onClick={props.handleClick} className="rounded-lg bg-[#242742] pt-[18px] pb-[14px] hover:bg-gradient-to-bl from-[#ff6a3a] to-[#ff527b] hover:shadow-[0px 16px 32px 0 rgba(255,97,85,0.5)]">
+          <span className="text-base font-bold text-center text-white font-RobertoBold">Subscribe to monthly newsletter</span>
+        </button>
+      </MediaQuery>
     </div>
   )
 }
