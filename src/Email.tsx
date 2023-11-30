71ee3241b43a506fed392acac9aee3f1c60a7dd1
Michael Ngo <michaelngo1337@yahoo.com>
2023-11-29 16:32:00 -0800

    change Email component to be a form

--- src/Email.tsx
+++ src/Email.tsx
@@ -4,10 +4,10 @@ const Email =  (props) => {
 
 
   return (
-    <div className="flex flex-col mt-[40px] font-Roberto">
-      <p className="text-xs font-bold text-left text-[#242742] mb-[8px]">Email address</p>
+    <form className="flex flex-col mt-[40px] font-Roberto">
+      <label className="text-xs font-bold text-left text-[#242742] mb-[8px]">Email address</label>
       <div className="rounded-lg border border-[#19182b]/25 mb-[24px]">
-        <p className="opacity-50 text-base text-left text-[#242742] pt-[16px] pb-[16px] pl-[24px]">email@company.com</p>
+        <input className="rounded-lg text-base text-left text-[#242742] pt-[16px] pb-[16px] pl-[24px] w-full" placeholder="email@company.com" />
       </div>
       <button onClick={props.handleClick} className="rounded-lg bg-[#242742] pt-[18px] pb-[14px] lg:hidden">
         <span className="text-base font-bold text-center text-white font-RobertoBold">Subscribe to monthly newsletter</span>
@@ -18,7 +18,7 @@ const Email =  (props) => {
           <span className="text-base font-bold text-center text-white font-RobertoBold">Subscribe to monthly newsletter</span>
         </button>
       </MediaQuery>
-    </div>
+    </form>
   )
 }
 
